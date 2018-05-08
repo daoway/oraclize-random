daf68ae555846afb816ddb71f89162fbc522dde7
stas.ostapenko@gmail.com <stas.ostapenko@gmail.com>
2018-05-09 01:51:55 +0300

    eth balance check

--- ext-contract-call.js
+++ ext-contract-call.js
@@ -5,14 +5,20 @@ const Random_number = contract(artifacts);
 Random_number.setProvider(web3.currentProvider);
 
 module.exports = function (callback) {
+    var accountIndex = 0;
     return Random_number.deployed().then(function (instance) {
         return web3.eth.sendTransaction({
-            from:web3.eth.accounts[1],
+            from:web3.eth.accounts[accountIndex],
             to:instance.address,
             value: web3.toWei(1, "ether"),
             gas: "2200000"
         });
     }).then(function (value) {
-        console.log(value);
+        console.log("tx hash : "+value);
+        return web3.eth.getBalance(web3.eth.accounts[accountIndex]);
+    }).then(function (value) {
+        return web3.fromWei(value);
+    }).then(function (value) {
+        console.log("eth balance :"+value);
     });
 };
